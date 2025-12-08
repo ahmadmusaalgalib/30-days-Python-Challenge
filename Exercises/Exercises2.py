b297bd32dde4e9196e3711d9e753e2d90e808989
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-09 03:45:28 +0600

    exercise2

---
+++ Exercises/Exercises2.py
@@ -0,0 +1,8 @@
+'''
+Tony is Srcretly a Superhero .ask him for his,
+then write the superhero name and show it on the terminal
+'''
+
+print("Tony what is your Superhero name?")
+name=input()
+print("SuperHero Name is"+" "+name)
