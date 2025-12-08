69f550d927b10bf424e1d5bc758763c43f78dfea
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-09 04:02:11 +0600

    type coversion

---
+++ Day2/Type_Conversion.py
@@ -0,0 +1,3 @@
+old_age=input("Enter your old age: ")
+new_age=int(old_age)+5 #int,float,str,bool are type conversion
+print(new_age)
---
+++ Day2/sum_of_2_numbers.py
@@ -0,0 +1,5 @@
+first_number = input("Enter the First number :")
+Second_number  = input("Enter the Second number :")
+
+sum= int(first_number) + int(Second_number)
+print("The sum is :"+ " " + str(sum))
