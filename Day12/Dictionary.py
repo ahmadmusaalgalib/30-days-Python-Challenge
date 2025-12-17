5c9809e0986f4b704bf8b6da04a53b0fbc669d48
Ahmed Musa Al Galib <geniuscuber1234@gmail.com>
2025-12-17 22:33:37 +0600

    Create Dictionary.py

---
+++ Day12/Dictionary.py
@@ -0,0 +1,11 @@
+marks={
+    "Chemistry" : 29,
+    "English" : 19,
+    "Physics" : 30,
+    "Mathematices" : 29,
+}
+
+print(marks)
+print(marks["English"])
+marks["Mathematices"]=38
+print(marks)
